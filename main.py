73b42381dc198b144b10a6e7333efb372e37f4b0
Nadisha Gautam <nadishagautam@gmail.com>
2023-12-29 12:33:18 -0500

    moved secret key

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from routes.auth import auth_routes
 from routes.recommendation import recommendation_routes
 
 app = Flask(__name__)
-app.secret_key = "1b377bc3fcc14f399573f6d07c0fef92"
+app.config.from_pyfile('config.py')
 
 app.register_blueprint(auth_routes)
 app.register_blueprint(recommendation_routes)
